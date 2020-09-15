d29fdcad023f0bc088aaf410bdf294ce8cc8ff1d
Vadim Sidorov(2nd PC) <vadsdrv@yandex.ru>
2020-09-15 12:17:33 +0300

    feat: add dream-team.js functionality

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,24 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+    
+membername = /[a-z\s][\w\s]{1,}/i;
+firstLetter = /[a-z]/i;
+let secretName = '';
+ 
+ if(Array.isArray(members) !== true) {
+    return false;
+ }
+ 
+ for (let i = 0; i < members.length; i++) { 
+    if(typeof(members[i]) !== 'string') continue;
+    if(membername.test(members[i]) !== true) return false;
+    secretName += members[i].match(firstLetter);      
+ }
+ 
+ return secretName.toLocaleUpperCase().split('').sort().join('');
 };
+
+
+
+
