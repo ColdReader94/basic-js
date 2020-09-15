a5fcc2f7f62f21fcb6ff03c47033c4dfe665fa35
Vadim Sidorov(2nd PC) <vadsdrv@yandex.ru>
2020-09-15 12:59:55 +0300

    fix: regular expression in dream-team.js

--- src/dream-team.js
+++ src/dream-team.js
@@ -3,7 +3,7 @@ const CustomError = require("../extensions/custom-error");
 module.exports = function createDreamTeam(members) {
     
 membername = /[a-z\s][\w\s]{1,}/i;
-firstLetter = /[a-z]/i;
+firstLetter = /[a-z\S]/i;
 let secretName = '';
  
  if(Array.isArray(members) !== true) {
