44575c7cced353cf96663f5bf033a96e00271972
Vadim Sidorov(2nd PC) <vadsdrv@yandex.ru>
2020-09-16 10:20:17 +0300

    fix: write solution that pass all tests of task

--- src/what-season.js
+++ src/what-season.js
@@ -1,14 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(date) {  
-    if(typeof(date) !== 'string' || 'number') throw Error('FAIL');
-    let value = new Date(date);    
-    if(typeof(value) !== Date) return 'Unable to determine the time of year!';
+module.exports = function getSeason(date) {      
+    if(date === null || date === undefined) return 'Unable to determine the time of year!';
+   
+    try {
+    date.getTime();    
+    } catch (err) {
+        throw new Error(err, err.message);
+    } 
     
+    let month = date.getMonth() + 1;
     
-    let month = value.getMonth();
-    if(month <= 2) return 'winter';
-    if(month <= 5) return 'spring';
-    if(month <= 8) return 'summer';
-    if(month <= 11) return 'autumn';
+    if(month === 12 || month < 3)return 'winter';
+    if(month >= 3 && month <= 5)return 'spring';
+    if(month >= 6 && month <= 8)return 'summer';
+    if(month >= 9 && month <= 11)return 'autumn';
+    else return false;
 };
