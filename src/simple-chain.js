bbb861e9675b90230c78d4a2f07bd3716b195a0a
Vadim Sidorov(2nd PC) <vadsdrv@yandex.ru>
2020-09-28 15:05:54 +0300

    feat: add solution for chainmaker task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,33 @@
 const CustomError = require("../extensions/custom-error");
 
+
 const chainMaker = {
+ arr: [],
+ string: '',
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.arr.length;
   },
-  addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {       
+     this.arr.push("( " + value + " )");
+     return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+      if(!this.arr[position]) { 
+         this.arr = [];
+         throw new Error('Out of range');
+      }
+      this.arr.splice(position - 1, 1);
+      return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+      this.arr.reverse();
+      return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+   this.string = this.arr.join('~~');
+   this.arr = [];
+   return this.string;
+  }  
 };
 
 module.exports = chainMaker;
