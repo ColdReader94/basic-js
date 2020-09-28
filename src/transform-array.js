b98b913705569a5ae5535d9ce890bcf5b468e500
Vadim Sidorov(2nd PC) <vadsdrv@yandex.ru>
2020-09-28 12:38:18 +0300

    feat: add solution for transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,42 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+    let newArr = [];
+    if(!Array.isArray(arr)) {
+        throw new Error('Not an array');
+    } else if(arr.length == 0) { 
+        return newArr;
+    };
+                
+    for(let i = 0; i < arr.length; i++) {
+        switch(arr[i]) {
+            case '--discard-next': {
+                 if(i + 1 < arr.length){
+                  i++;
+                };
+            break;
+            }
+            case '--discard-prev': {
+                if(i - 1 >= 0 && arr[i - 2] !== '--discard-next'){
+                newArr.pop();
+                }
+            break;
+            }
+            case '--double-next': {
+                if(i + 1 < arr.length){
+                newArr.push(arr[i + 1]);
+                }
+            break;
+            }
+             case '--double-prev': {
+                if(i - 1 >= 0 && arr[i - 2] !== '--discard-next'){
+                newArr.push(arr[i - 1]);
+                }
+            break;
+            } 
+            default: {
+                newArr.push(arr[i]);   
+            }
+        }};
+        return newArr;
+    };
