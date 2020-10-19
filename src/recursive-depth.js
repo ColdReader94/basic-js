519050290a3eefb4b7ca6609eac5ea96658c4d9f
Vadim Sidorov <myEmail@example.com>
2020-10-19 22:08:42 +0300

    feat: complete task

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -2,21 +2,15 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
   calculateDepth(arr) {
-      this.counter = 0;
- if(!arr instanceof Array) {
-      return this.counter;
-   } else { 
-   this.counter++;
-   for (let i = 0; i < arr.length; i++) {       
-       if (arr[i] instanceof Array) {
-       break;
-       this.counter++;       
-       this.calculateDepth(arr[i]);     
-       }
-   }
-   };
-   return this.counter;
-  }
+    let counter = 0;
+    if (Array.isArray(arr)) {
+        for (let i = 0; i < arr.length; i++) {
+            counter = Math.max(counter, this.calculateDepth(arr[i]));
+        }
+        counter++;
+    }
+    return counter;
+}
 };
 
 
