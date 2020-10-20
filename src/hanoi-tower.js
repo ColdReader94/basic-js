3e6e9d02f5efbb61ae7acf9909c41b8ee3d9e631
Vadim Sidorov <myEmail@example.com>
2020-10-20 13:14:32 +0300

    feat: complete hanoi

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+let secondsInhour = 60 * 60 //секунд в 1 часу
+
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+  let turnsNeed = Math.pow(2, disksNumber) - 1; // ходов понадобится
+  let secondsPerTurn = turnsSpeed / secondsInhour; // секунд затрачено на 1 ход
+  let timeNeed = Math.floor(turnsNeed / secondsPerTurn); // время понадобится на необходимое количество ходов
+  let object = {
+      turns: turnsNeed,
+      seconds: timeNeed
+  };
+  console.log(object);
+  return object;
+}
