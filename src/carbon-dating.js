1ce8131f740607faaed93b4eb976ffa3dda48488
Vadim Sidorov <myEmail@example.com>
2020-10-20 12:05:05 +0300

    feat: complete carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,10 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15; 
+const HALF_LIFE_PERIOD = 5730;
+const K = 0.693 / HALF_LIFE_PERIOD;
 
 module.exports = function dateSample(sampleActivity) {
- if (typeof sampleActivity !== 'string') return false;
-
-
+if (typeof sampleActivity !== 'string') return false;
+let currentActivity = Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity));
+let age = Math.ceil(currentActivity / K);
+if(Number.isInteger(age) && age >= 0) {
+    return age;
+} else return false;
 };
